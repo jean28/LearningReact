0db70da9b71b6fe41ba8a194228e01dffb9e3bee
jean28 <jean.mendez2@upr.edu>
2018-10-02 00:08:36 -0400

    finished examples from chapter 3

--- chapter_03_functional_programming/ticking_clock_functional.js
+++ chapter_03_functional_programming/ticking_clock_functional.js
@@ -27,13 +27,17 @@ const appendAMPM = clockTime => ({
 	ampm: (clockTime.hours >= 12) ? "PM" : "AM"
 })
 
-/* Takes a target function and returns a function that will send a time to the target.
-   In this example the target will be console.log. */
+/* 
+   Takes a target function and returns a function that will send a time to the target.
+   In this example the target will be console.log. 
+*/
 const display = target => time => target(time)
 
-/* Takes a template string and uses it to return clock time formatted based upon the criteria
+/* 
+   Takes a template string and uses it to return clock time formatted based upon the criteria
    from the string. In this example, the template is "hh:mm:ss tt". From there, 
-   formatClock will replace the placeholders with hours, minutes, seconds, and time of day. */
+   formatClock will replace the placeholders with hours, minutes, seconds, and time of day. 
+*/
 const formatClock = format =>
 	time => 
 		format.replace("hh", time.hours)
@@ -41,12 +45,55 @@ const formatClock = format =>
 			  .replace("ss", time.seconds)
 			  .replace("tt", time.ampm)
 
-/* Takes an object's key as an argument and prepends a zero to the value stored 
+/* 
+   Takes an object's key as an argument and prepends a zero to the value stored 
    under that objects key. It takes in a key to a specific field and prepends values
-   with a zero if the value is less than 10. */
+   with a zero if the value is less than 10. 
+*/
 const prependZero = key => clockTime => ({
 	...clockTime,
 	[key]: (clockTime[key] < 10) ?
 		"0" + clockTime[key] : 
 		clockTime[key]
 })
+
+/*
+	A single function that will take clock time as an argument and transforms it 
+	into civilian time by using both civilian hours.
+*/
+const convertToCivilianTime = clockTime => 
+	compose(
+		appendAMPM,
+		civilianHours
+	)(clockTime)
+
+/*
+	A single function that will take civilian clock time and make sure the hours, 
+	minutes, and seconds display double digits by prepending zeroes where needed.
+*/
+const doubleDigits = civilianTime =>
+	compose(
+		prependZero("hours"),
+		prependZero("minutes"),
+		prependZero("seconds")
+	)(civilianTime)
+
+/*
+	Starts the clock by setting an interval that will invoke a callback every second.
+	The callback is composed using all of our functions. Every second the console is 
+	cleared, currentTime obtained, converted, civilianized, formatted, and displayed.
+*/
+const startTicking = () =>
+	setInterval(
+		compose(
+			clear,
+			getCurrentTime,
+			serializeClockTime,
+			convertToCivilianTime,
+			doubleDigits,
+			formatClock("hh:mm:ss tt"),
+			display(log)
+		), oneSecond()
+	)
+
+startTicking()
